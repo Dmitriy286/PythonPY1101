897d0b55f672e7cbd4591122b202937628865252
Maroders <DShishlov@gmail.com>
2021-11-22 13:34:49 +0300

    22.11.2021

--- Занятие2/Домашние_задания/task3_2/main.py
+++ Занятие2/Домашние_задания/task3_2/main.py
@@ -1,3 +1,20 @@
+def decorator(fn):
+    def wraper(*args):
+        if type(*args) == int:
+            print("Positional argument is integer")
+        else:
+            print("Positional argument is not integer")
+        fn(*args)
+    return wraper
+
+@decorator
+def print_(a):
+    print(a)
+
+@decorator
+def print_1(b):
+    print(b)
+
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    print_(8)
+    print_1("str")
