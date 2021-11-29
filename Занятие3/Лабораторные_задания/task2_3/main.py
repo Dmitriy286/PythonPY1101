93fb456febbd2d6c1e08f0788494b1a57abdc743
Maroders <DShishlov@gmail.com>
2021-11-29 15:06:01 +0300

    29.11.2021

--- Занятие3/Лабораторные_задания/task2_3/main.py
+++ Занятие3/Лабораторные_задания/task2_3/main.py
@@ -3,10 +3,11 @@ import json
 
 def task():
     filename = "input.json"
-    # TODO считать содержимое JSON файла
+    with open(filename, "r") as f:# TODO считать содержимое JSON файла
+        jsn_str = json.load(f)
 
-    return ...  # TODO найти максимальный элемент по ключу score
-
+    return max(jsn_str, key=lambda x: x["score"])  # TODO найти максимальный элемент по ключу score
+#fixme как это работает?
 
 if __name__ == "__main__":
     print(task())
--- Занятие3/Лабораторные_задания/task2_4/main.py
+++ Занятие3/Лабораторные_задания/task2_4/main.py
@@ -3,10 +3,13 @@ import json
 
 def task():
     filename = "input.json"
-    # TODO считать содержимое JSON файла
-
-    gen_exr = ...  # TODO записать выражение-генератор возвращающее значение по ключу contains_improvement_appeals
+    with open(filename, "r") as f:# TODO считать содержимое JSON файла
+        jsn_fl = json.load(f)
+    gen_exr = (jsn_fl[i]["contains_improvement_appeals"] for i in range(len(jsn_fl)))  # TODO записать выражение-генератор возвращающее значение по ключу contains_improvement_appeals
+    # gen_exr = (item["contains_improvement_appeals"] for item in json_data)
+    #fixme
     return sum(gen_exr)
+
 
 
 if __name__ == "__main__":
