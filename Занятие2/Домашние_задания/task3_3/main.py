5dbb7bc6df6d4277d54da1a54706fda12efd686d
Maroders <DShishlov@gmail.com>
2021-11-30 14:04:32 +0300

    30.11.2021

--- Занятие2/Домашние_задания/task3_3/main.py
+++ Занятие2/Домашние_задания/task3_3/main.py
@@ -1,3 +1,17 @@
+def decorator(fn):
+    def wrapper(*args, **kwargs):
+        iter_or_not = (i for i in args)
+        return fn()
+        else:
+        raise TypeError("Объект <название или номер позиционного аргумента> "
+                        "<значение аргумента> не является итерируемым")
+
+@decorator
+def func_1(a, b, c, d):
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    func_1([1, 2, 3], {1, 3, 4}, xy=[4, 5, 6], yx=[6, "d", "a"])
--- Занятие3/Лабораторные_задания/task2_5/main.py
+++ Занятие3/Лабораторные_задания/task2_5/main.py
@@ -1,15 +1,31 @@
-import json
+# import json
+#
+#
+# def task():
+#     filename = "input.json"
+#     with open(filename) as f:
+#         json_data = json.load(f)
+#
+#     map_iterator = map(lambda item: item["score"] * item["weight"], json_data)
+#     return sum(map_iterator)
+#
+#
+# if __name__ == "__main__":
+#     result = task()
+#     print(f"{result:.3f}")
 
+import json
 
 def task():
     filename = "input.json"
-    with open(filename) as f:
-        json_data = json.load(f)
-
-    map_iterator = map(lambda item: item["score"] * item["weight"], json_data)
-    return sum(map_iterator)
-
+    with open(filename, "r") as f:
+        jsn_str = json.load(f)
+        iter_ = map(lambda x: x["score"] * x["weight"], jsn_str)
+        return sum(iter_)
 
 if __name__ == "__main__":
+    # print(task())
     result = task()
     print(f"{result:.3f}")
+
+
